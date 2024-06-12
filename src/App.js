53ff8b802008ef17cba8ddad4b1c92677f7a015f
NidhiJain08 <munmundangi@gmail.com>
2024-06-12 11:34:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import axios from  'axios';
+import './index.css'; // Ensure you import your CSS file
+function App(){
 
-function App() {
+  const [data,setData]=useState({})
+  const [location,setLocation]=useState('')
+
+  const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid={API key}`
+  const handleKeyDown=(event)=>{
+    if(event.key==='Enter'){
+      axios.get(url).then((response)=>{
+        setData(response.data)
+        console.log(response.data)
+     })
+     setLocation('')
+    }
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className='app'>
+    <div className="search">
+    <input type='text' 
+    value={location} 
+    onChange={event=> setLocation(event.target.value)} 
+    placeholder='Enter city'
+    onKeyDown={handleKeyDown} />
+ 
     </div>
+    
+    <div className="container">
+      <div className="top">
+         <div className="location">
+          <p>{data.name}</p>
+         </div>
+         <div className="temp">
+          {data.main ? <h1>{data.main.temp}°C</h1> : null}
+         </div>
+         <div className="description">
+           {data.weather ? <p>{data.weather[0].description}</p> : null}
+         </div>
+      </div>
+      {!data.name && (
+          <div className="welcome">
+            <h2>Weather Web App</h2>
+            <p>Enter a city name to get the current weather information.</p>
+          </div>
+        )}
+    
+      {
+      data.name !== undefined && (<div className="bottom">
+        <div className="feels">
+        {data.main ? <p>{data.main.feels_like}</p> : null}
+        <p>Feels like</p>
+        </div>
+        <div className="humidity">
+        {data.main ? <p>{data.main.humidity}</p> : null}
+         <p>Humidity</p>
+        </div>
+        <div className="wind">
+        {data.wind ? <p>{data.wind.speed} MPH</p> : null}
+         <p>Wind speed</p>
+        </div>
+      </div>)
+      }
+    </div>
+
+  </div>
   );
 }
-
 export default App;
